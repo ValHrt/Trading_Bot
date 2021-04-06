6499b81587517222b8ce9be036199e1282df5699
Valentin Henriot <59604857+ValHrt@users.noreply.github.com>
2021-04-06 18:38:27 +0200

    tkinter and logging modules configuration

---
+++ main.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+import logging
+
+logger = logging.getLogger()
+
+logger.setLevel(logging.INFO)
+
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)s :: %(message)s')
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler("info.log")
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.mainloop()
