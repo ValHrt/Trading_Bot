ace580e827dd81076ab481041f94191bcd4d74c9
Valentin Henriot <59604857+ValHrt@users.noreply.github.com>
2021-04-06 23:18:53 +0200

    Get contracts from Binance API

--- binance_futures.py
+++ binance_futures.py
@@ -1,3 +1,19 @@
 import logging
+import requests
+import pprint
 
 logger = logging.getLogger()
+
+
+def get_contracts():
+    response_object = requests.get("https://testnet.binancefuture.com/fapi/v1/exchangeInfo")
+
+    contracts = []
+
+    for contract in response_object.json()['symbols']:
+        contracts.append(contract['pair'])
+
+    return contracts
+
+
+print(get_contracts())
