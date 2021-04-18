9e77d58344d8160853c0795ae458c3caffe18b4f
Valentin Henriot <59604857+ValHrt@users.noreply.github.com>
2021-04-18 14:24:14 +0200

    Watchlist prices

--- interface/root_component.py
+++ interface/root_component.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import time
+import logging
 
 from connectors.binance_futures import BinanceFuturesClient
 from connectors.bitmex import BitmexClient
@@ -7,6 +7,9 @@ from connectors.bitmex import BitmexClient
 from interface.styling import *
 from interface.logging_component import Logging
 from interface.watchlist_component import Watchlist
+
+
+logger = logging.getLogger()
 
 
 class Root(tk.Tk):
@@ -36,6 +39,8 @@ class Root(tk.Tk):
 
     def _update_ui(self):
 
+        # Logs
+
         for log in self.bitmex.logs:
             if not log["displayed"]:
                 self._logging_frame.add_log(log['log'])
@@ -45,6 +50,51 @@ class Root(tk.Tk):
             if not log["displayed"]:
                 self._logging_frame.add_log(log['log'])
                 log["displayed"] = True
+
+        # Watchlist prices
+
+        try:
+            for key, value in self._watchlist_frame.body_widgets["symbol"].items():
+
+                symbol = self._watchlist_frame.body_widgets["symbol"][key].cget("text")
+                exchange = self._watchlist_frame.body_widgets["exchange"][key].cget("text")
+
+                if exchange == "Binance":
+                    if symbol not in self.binance.contracts:
+                        continue
+
+                    if symbol not in self.binance.prices:
+                        self.binance.get_bid_ask(self.binance.contracts[symbol])
+                        continue
+
+                    precision = self.binance.contracts[symbol].price_decimals
+
+                    prices = self.binance.prices[symbol]
+
+                elif exchange == "Bitmex":
+                    if symbol not in self.bitmex.contracts:
+                        continue
+
+                    if symbol not in self.bitmex.prices:
+                        continue
+
+                    precision = self.bitmex.contracts[symbol].price_decimals
+
+                    prices = self.bitmex.prices[symbol]
+
+                else:
+                    continue
+
+                if prices['bid'] is not None:
+                    price_str = "{0:.{prec}f}".format(prices["bid"], prec=precision)
+                    self._watchlist_frame.body_widgets['bid_var'][key].set(price_str)
+
+                if prices['ask'] is not None:
+                    price_str = "{0:.{prec}f}".format(prices["ask"], prec=precision)
+                    self._watchlist_frame.body_widgets['ask_var'][key].set(price_str)
+
+        except RuntimeError as e:
+            logger.error("Erreur en effectuant la boucle sur le dictionnaire: %s", e)
 
         self.after(1500, self._update_ui)
 
