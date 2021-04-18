95804fd3ec49ab22d8b974846917e909e863c573
Valentin Henriot <59604857+ValHrt@users.noreply.github.com>
2021-04-18 14:23:40 +0200

    Changing one argument in module get_contracts

--- connectors/binance_futures.py
+++ connectors/binance_futures.py
@@ -93,7 +93,7 @@ class BinanceFuturesClient:
 
         if exchange_info is not None:
             for contract_data in exchange_info["symbols"]:
-                contracts[contract_data['pair']] = Contract(contract_data, "binance")
+                contracts[contract_data['symbol']] = Contract(contract_data, "binance")
 
         return contracts
 
